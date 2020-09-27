6da3926d2a93c0bd3ce2af309c755253c0538875
Abril-Barrera <abril.barrera.moreno@gmail.com>
2020-09-27 16:09:20 -0500

    Added menu to selec changes of the environment. Added functions to the agent.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Environment():
         self.world = None
 
     def createRandom(self):
-        self.width = random.randint(1, 25)
-        self.height = random.randint(1, 25)        
+        self.width = random.randint(1, 20)
+        self.height = random.randint(1, 20)        
         self.matrix = np.random.randint(0,2, size = (self.width, self.height))
 
         self.world = prettytable.PrettyTable(header=False)
@@ -23,35 +23,54 @@ class Environment():
         self.vacuum = VacuumCleaner(self.width, self.height)
 
     def createCustom(self):
-        self.width = 0
-        self.height = 0
+        self.width = 3
+        self.height = 3
+        cont = 0
 
-        while True:
-            try:
-                self.width = int(input("Ingrese la altura del ambiente: "))
-                self.height = int(input("Ingrese la anchura  del ambiente: "))        
-            except ValueError:
-                print("Solo enteros! intente de nuevo.")
-                continue
-        
-        while True:
-            flag1 = True
-            flag2 = True
+        while cont == 0:
+
+            print("¿Cuáles valores quieres modificar?")
+            print("**Si no modificas el tamaño será de 3x3 por default. Si no modificas la posición inicial, será (0,0) por default.**")
+            print("")
+            print("/t 1) Tamaño")
+            print("/t 2) Posición inicial")
+            print("/t 3) Salir")
+            opt = int(input())
+
+            if opt == 1:
+                while True:
+                    try:
+                        self.width = int(input("Ingrese la altura del ambiente: "))
+                        self.height = int(input("Ingrese la anchura  del ambiente: "))        
+                    except ValueError:
+                        print("Solo enteros! intente de nuevo.")
+                        continue
             
-            print("Ingrese la posición inicial de la aspiradora en")
-            self.vacuum.x = int(input("X: "))
-            if self.vacuum.x <= 0 or self.vacuum.x >= self.width:
-                print('ERROR: Ingrese valores validos')
-                flag1 = False
+            if opt == 2:
+                while True:
+                    flag1 = True
+                    flag2 = True
+                    
+                    print("Ingrese la posición inicial de la aspiradora en: ")
+                    self.vacuum.x = int(input("X: "))
+                    if self.vacuum.x <= 0 or self.vacuum.x >= self.width:
+                        print('ERROR: Ingrese valores validos')
+                        flag1 = False
 
-            self.vacuum.y = int(input("Y: "))
-            if self.vacuum.y <= 0 or self.vacuum.y >= self.width:
-                print('ERROR: Ingrese valores validos')
-                flag2 = False
+                    self.vacuum.y = int(input("Y: "))
+                    if self.vacuum.y <= 0 or self.vacuum.y >= self.width:
+                        print('ERROR: Ingrese valores validos')
+                        flag2 = False
 
-            if flag1 and flag2:
+                    if flag1 and flag2:
+                        break
+            if opt == 3:
+                cont = 1
                 break
 
+            if opt <= 0 or opt >= 4:
+                print("Selecciona una opción válida.")
+            
 
 class VacuumCleaner():
     def __init__(self, width, height):
@@ -66,26 +85,36 @@ class VacuumCleaner():
 
         self.sensors = {'up': False, 'down': False,  'left': True, 'right': True}
         
-    def create(self):
+    def createAgent(self):
         pass
 
-    def move(self):
+    def moveRight(self):
         pass
 
-    def clean(self):
+    def moveLeft(self):
         pass
 
+    def moveUp(self):
+        pass
+
+    def moveDown(self):
+        pass
+
+    def cleanDirt(self):
+        pass
+
+    def detectDirt(self):
+        pass
 
 def main():
     env = Environment()
     opt = menu()
     if opt == 1:
         env.createRandom()
+        print("Estes es tu nuevo ambiente: ")
+        print(env.matrix)
     elif opt == 2:
         env.createCustom()
-
-    print(env.world)
-    
 
 def menu():
     print("Bienvenido al entorno de aspiradora. Escoge una opción: ")
@@ -96,7 +125,6 @@ def menu():
     if opt > 2 or opt < 0:
         print("Opción no válida, escoge de nuevo")
         menu()
-
     return opt
 
 
